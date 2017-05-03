2b08c880fe4b2d16dda849efb547c1278179a8c9
harunurkst <harun1393@gmail.com>
2017-05-03 12:24:14 +0600

    added home template

--- testapi/urls.py
+++ testapi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include('api.urls')),
+    url(r'^home', views.home, name="home"),
 ]
